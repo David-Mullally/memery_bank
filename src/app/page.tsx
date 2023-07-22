7e82c4ec9325def070ee091fcdb4fce741de6944
Dave Mullally <mullallydavid7@gmail.com>
2023-07-22 12:10:34 +0200

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,6 @@ const HomePage: React.FC = () => {
     <div className="flex flex-wrap w-[100vw] h-[100vh] bg-gray-500">
       <UploadDownloadImageComponent />
       <EditImageComponent />
-      <div>
-      <h1>Draggable Element Example</h1>
-      <DraggableElement defaultPosition={{ x: 100, y: 100 }} />
-    </div>
     </div>
   );
 };
